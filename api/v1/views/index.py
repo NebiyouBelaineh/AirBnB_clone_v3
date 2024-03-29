eb89274232cee5c9d4e50cf6f96cb57d9a9f4a06
Dridi Chaith <dridi.chaith@gmail.com>
2024-03-29 22:54:41 +0100

    task 4 pycodestyle

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,7 +4,6 @@ from api.v1.views import app_views
 from flask import jsonify
 
 
-
 @app_views.route('/status')
 def status():
     """show status as a json"""
@@ -15,7 +14,7 @@ def status():
 @app_views.route('/stats')
 def stats():
     """Returns the number of each objects by type"""
-    
+
     from models import storage
     from models.amenity import Amenity
     from models.city import City
@@ -23,10 +22,10 @@ def stats():
     from models.review import Review
     from models.state import State
     from models.user import User
-    
-    classes = {"amenities": Amenity, "cities": City,
-			"places": Place, "reviews": Review, "states": State, "users": User}
- 
+
+    classes = {"amenities": Amenity, "cities": City, "places": Place,
+               "reviews": Review, "states": State, "users": User}
+
     obj = {}
     for key, values in classes.items():
         obj[key] = storage.count(values)
