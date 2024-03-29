bbed65adc776e0d50a5bc60717b08797688ac325
Dridi Chaith <dridi.chaith@gmail.com>
2024-03-30 00:03:00 +0100

    task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,6 +2,7 @@
 """Index module"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status', strict_slashes=False)
@@ -14,17 +15,12 @@ def status():
 @app_views.route('/stats', strict_slashes=False)
 def stats():
     """Returns the number of each objects by type"""
-
-    from models import storage
-    from models.amenity import Amenity
-    from models.city import City
-    from models.place import Place
-    from models.review import Review
-    from models.state import State
-    from models.user import User
-
-    classes = {"amenities": Amenity, "cities": City, "places": Place,
-               "reviews": Review, "states": State, "users": User}
+    classes = {"amenities": 'Amenity',
+               "cities": 'City',
+               "places": 'Place',
+               "reviews": 'Review',
+               "states": 'State',
+               "users": 'User'}
 
     obj = {}
     for key, values in classes.items():
