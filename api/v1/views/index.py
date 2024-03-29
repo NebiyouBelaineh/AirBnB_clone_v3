3f618f2906f6f0f4bfc8ab8a285c67395f649264
Dridi Chaith <dridi.chaith@gmail.com>
2024-03-29 23:23:45 +0100

    task 4 strict slashes

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,14 +4,14 @@ from api.v1.views import app_views
 from flask import jsonify
 
 
-@app_views.route('/status')
+@app_views.route('/status', strict_slashes=False)
 def status():
     """show status as a json"""
     status = jsonify({"status": "OK"})
     return status
 
 
-@app_views.route('/stats')
+@app_views.route('/stats', strict_slashes=False)
 def stats():
     """Returns the number of each objects by type"""
 
