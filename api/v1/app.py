e326d90855d91ecc45b2c56fa6409ff43b3f96c5
Dridi Chaith <dridi.chaith@gmail.com>
2024-03-30 00:49:36 +0100

    task 5

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """flask app for rendering api"""
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -14,6 +14,12 @@ app.register_blueprint(app_views)
 def tearDown_appcontext(exception):
     """Calls stroage.close to release storage engine resources"""
     storage.close()
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    """404 error handler"""
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
