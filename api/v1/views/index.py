4779ca89311da982358f738c630f741c228e036f
Dridi Chaith <dridi.chaith@gmail.com>
2024-03-29 22:47:03 +0100

    task 4 rearange imports

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,16 +2,7 @@
 """Index module"""
 from api.v1.views import app_views
 from flask import jsonify
-from models import storage
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
 
-classes = {"amenities": Amenity, "cities": City,
-           "places": Place, "reviews": Review, "states": State, "users": User}
 
 
 @app_views.route('/status')
@@ -24,6 +15,18 @@ def status():
 @app_views.route('/stats')
 def stats():
     """Returns the number of each objects by type"""
+    
+    from models import storage
+    from models.amenity import Amenity
+    from models.city import City
+    from models.place import Place
+    from models.review import Review
+    from models.state import State
+    from models.user import User
+    
+    classes = {"amenities": Amenity, "cities": City,
+			"places": Place, "reviews": Review, "states": State, "users": User}
+ 
     obj = {}
     for key, values in classes.items():
         obj[key] = storage.count(values)
