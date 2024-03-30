05152288ff07ca52dbdf3fa1977ccdd07fdda82e
Dridi Chaith <dridi.chaith@gmail.com>
2024-03-30 16:41:00 +0100

    task 6

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -14,15 +14,16 @@ def hbnb_status():
 
 @app_views.route('/stats')
 def hbnb_stats():
+    from models.amenity import Amenity
+    from models.city import City
+    from models.place import Place
+    from models.review import Review
+    from models.state import State
+    from models.user import User
     """Returns the number of each objects by type"""
-    classes = {"amenities": 'Amenity',
-               "cities": 'City',
-               "places": 'Place',
-               "reviews": 'Review',
-               "states": 'State',
-               "users": 'User'}
-
-    obj = {}
-    for key, values in classes.items():
-        obj[key] = storage.count(values)
-    return jsonify(obj)
+    return jsonify({"amenities": storage.count(Amenity),
+                    "cities": storage.count(City),
+                    "places": storage.count(Place),
+                    "reviews": storage.count(Review),
+                    "states": storage.count(State),
+                    "users": storage.count(User)})
--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -29,6 +29,7 @@ def get_state(state_id):
 def delete_state(state_id):
     """delete states as a json"""
     state = storage.get("State", state_id)
+    print(state)
     if state is None:
         abort(404)
     state.delete()
