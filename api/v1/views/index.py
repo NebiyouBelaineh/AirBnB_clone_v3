0d875941530db0b79223a87330a62ae9e230c255
Dridi Chaith <dridi.chaith@gmail.com>
2024-03-30 16:49:00 +0100

    task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,29 +1,32 @@
 #!/usr/bin/python3
-"""Index module"""
-from api.v1.views import app_views
+"""
+Defines a route to show the status of the web app
+Defines a route to show the states of the db
+"""
 from flask import jsonify
+from api.v1.views import app_views
 from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
-@app_views.route('/status')
-def hbnb_status():
-    """show status as a json"""
-    status = jsonify(status="OK")
-    return status
+@app_views.route('/status', strict_slashes=False)
+def status():
+    """ returns the status of the service """
+    return {'status': 'OK'}
 
 
-@app_views.route('/stats')
-def hbnb_stats():
-    from models.amenity import Amenity
-    from models.city import City
-    from models.place import Place
-    from models.review import Review
-    from models.state import State
-    from models.user import User
-    """Returns the number of each objects by type"""
-    return jsonify({"amenities": storage.count(Amenity),
-                    "cities": storage.count(City),
-                    "places": storage.count(Place),
-                    "reviews": storage.count(Review),
-                    "states": storage.count(State),
-                    "users": storage.count(User)})
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    """ that retrieves the number of each objects by type """
+    return jsonify({'amenities': storage.count(Amenity),
+                    'cities': storage.count(City),
+                    'places': storage.count(Place),
+                    'reviews': storage.count(Review),
+                    'states': storage.count(State),
+                    'users': storage.count(User),
+                    })
